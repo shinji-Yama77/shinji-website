c2788e87e21add0e7afa8cd899964591d6992a6b
Shinji Yamashita <syamas@uw.edu>
2021-12-17 15:35:10 +0800

    finished tailwind

---
+++ personal-website/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <button className="rounded-lg p-6 text-white bg-blue-500 shadow-lg shadow-blue-500/50">
+        Subscribe
+      </button>
+    </div>
+  );
+}
+
+export default App;
